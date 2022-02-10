c01bbdeeac0e0578becc8498a7999a712b8eb033
MirzaMubashshera <07.mirzamubashshera@gmail.com>
2022-02-10 18:52:21 +0530

    modiefy

--- home.js
+++ home.js
@@ -1,6 +1,12 @@
 let url = "https://developerfunnel.herokuapp.com/location"
 let hotelUrl = "https://developerfunnel.herokuapp.com/hotels?city="
+function test(){
+    document.getElementById('coupon').style.visibility="visible"
+}
 
+function closeDiv(){
+    document.getElementById('coupon').style.visibility="hidden"
+}
 function getCity(){
     fetch(url)
     .then((res) => res.json())
